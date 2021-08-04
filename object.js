ee7e008c54fa6cbe20a7b26374c0b84ccb9285bc
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-04 23:08:12 +0600

    multiple object and properties example added

---
+++ object.js
@@ -0,0 +1,24 @@
+//object-1
+var student1 = {
+    id: 115,
+    name: 'Ety Rabeya',
+    marks: 81,
+    phone: 36899774
+};
+
+//object-2
+ var mobile ={
+     color: 'black',
+     Price: 19000,
+     screenSize:5,
+     storage: '16GB'
+ };
+ 
+//object-3
+ var book={
+     title='Start with why', //egula object property each 
+     author: 'Simon sinak',
+     price: 200,
+     pages: 246,
+     publisher: 'penguin'
+ }
