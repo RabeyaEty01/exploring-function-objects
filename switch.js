b1694a62ccab2d108d27f0cd0e1405ec66c4f575
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-04 23:49:42 +0600

    switch & if else difference

---
+++ switch.js
@@ -0,0 +1,44 @@
+var color = 'yellow';
+//if else condition diye
+if (color == 'blue') {
+    console.log('color is blue');
+
+}
+else if (color == 'green') {
+    console.log('color is green');
+}
+
+else if (color == 'yellow') {
+    console.log('color is yellow');
+}
+
+else if (color == 'red') {
+    console.log('color is red');
+}
+else {
+    console.log('color is black');
+}
+
+//switch diye 
+switch (color) {
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'green':
+        console.log('color is green');
+        break;
+    case 'white':
+        console.log('color is white');
+        break;
+    case 'yellow':
+        console.log('color is yellow');
+        break;
+
+    default:
+        console.log('color is black');
+}
+
+//if else use korle jodi onkgula likhi specific khetre tahole one by one kore agay thn specific value tare paile output dey . R switch case er khetre one by one check korena . ek laaf e main specific tate chole jay thn output show kore
