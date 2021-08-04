7e8af6e69dbb9a72f61366390066f81dcee96aa3
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-04 16:48:10 +0600

    function added

---
+++ function.js
@@ -0,0 +1,13 @@
+//function declaration
+function startFan(){
+    console.log('walk towards the swith');
+    console.log('press the swith');
+}
+//call the function
+//startFan();
+
+console.log('Eat breakfast');
+console.log('Drink tea');
+console.log('take a shower');
+startFan();
+startFan();
